6827548a20e28715b2c4bb05d0503f034d3bddbd
diogjunior100 <diogjunior10071@gmail.com>
2025-01-24 17:53:02 -0300

    Altera nome dos arquivos

--- src/c++/cliente.cpp
+++ src/c++/cliente.cpp
@@ -3,51 +3,64 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
+#include <libpq-fe.h>
 
 #define PORT 12345  
+
 using namespace std;
 
 int main() {
-    int server_fd, new_socket;
-    struct sockaddr_in address;
-    int addrlen = sizeof(address);
-    const char *message = "10"; 
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char buffer[1024] = {0};
 
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("Falha ao criar o socket");
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        cerr << "Falha ao criar o socket" << endl;
         return -1;
     }
 
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;  // Aceitar qualquer endereço de IP
-    address.sin_port = htons(PORT); 
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
 
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("Falha ao fazer bind");
+    // 192.168.15.13 ou qualquer endereço da rede conectada
+    if (inet_pton(AF_INET, "192.168.15.3", &serv_addr.sin_addr) <= 0) {
+        cerr << "Endereço inválido ou não pode conectar ao servidor" << endl;
         return -1;
     }
 
-    while (1){
-        if (listen(server_fd, 3) < 0) {
-            perror("Falha ao escutar");
-            return -1;
-        }
-
-        cout << "Aguardando conexão..." << endl;
-
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
-            perror("Falha ao aceitar a conexão");
-            return -1;
-        }
-
-        cout << "Conexão estabelecida!" << endl;
-
-        send(new_socket, message, strlen(message), 0);
-        cout << "Dados enviados: " << message << endl;
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        cerr << "Falha na conexão" << endl;
+        return -1;
     }
 
-    close(new_socket);
-    close(server_fd);
+    read(sock, buffer, sizeof(buffer));
+    cout << "Dados recebidos: " << buffer << endl;
+
+    PGconn *conn = PQconnectdb("user=postgres password=postgres host=localhost port=5432");
+
+    if (PQstatus(conn) != CONNECTION_OK) {
+        cerr << "Falha na conexão com o banco de dados: " << PQerrorMessage(conn) << endl;
+        PQfinish(conn);
+        return -1;
+    }
+
+    string query = "INSERT INTO dados (valor) VALUES (" + string(buffer) + ")";
+    PGresult *res = PQexec(conn, query.c_str());
+
+    if (PQresultStatus(res) != PGRES_COMMAND_OK) {
+        cerr << "Falha na execução da query: " << PQerrorMessage(conn) << endl;
+        PQclear(res);
+        PQfinish(conn);
+        return -1;
+    }
+
+    cout << "Dados inseridos no banco de dados com sucesso!" << endl;
+
+    PQclear(res);
+    PQfinish(conn);
+
+    close(sock);
 
     return 0;
 }
